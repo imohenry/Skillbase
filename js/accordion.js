beaa8da2ed414970048e21f044699bcf7f5ee472
Imoh Henry <henryimoh2@gmail.com>
2023-05-12 11:18:30 +0100

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -12,3 +12,11 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+
+    function openForm() {
+      document.getElementById("popupForm").style.display = "block";
+    }
+    function closeForm() {
+      document.getElementById("popupForm").style.display = "none";
+    }
